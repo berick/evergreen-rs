236930bbe7f7f76ae885242bef8ddab5f42d3dc6
Bill Erickson <berickxx@gmail.com>
2022-02-03 17:59:19 -0500

    EG rust
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- src/idl.rs
+++ src/idl.rs
@@ -1,6 +1,18 @@
 use std::collections::HashMap;
+use std::fmt;
 use std::fs;
+use log::{trace};
 use roxmltree;
+
+const OILS_NS_BASE: &str = "http://opensrf.org/spec/IDL/base/v1";
+const OILS_NS_OBJ: &str = "http://open-ils.org/spec/opensrf/IDL/objects/v1";
+const OILS_NS_OBJ_PREFIX: &str = "oils_obj";
+const OILS_NS_PERSIST: &str = "http://open-ils.org/spec/opensrf/IDL/persistence/v1";
+const OILS_NS_PERSIST_PREFIX: &str = "oils_persist";
+const OILS_NS_REPORTER: &str = "http://open-ils.org/spec/opensrf/IDL/reporter/v1";
+const OILS_NS_REPORTER_PREFIX: &str = "reporter";
+
+const AUTO_FIELDS: [&str; 3] = ["isnew", "ischanged", "isdeleted"];
 
 pub enum DataType {
     Int,
@@ -20,6 +32,39 @@ impl DataType {
     }
 }
 
+impl Into<&'static str> for DataType {
+    fn into(self) -> &'static str {
+        match self {
+            Self::Int 		=> "int",
+            Self::Float 	=> "float",
+            Self::Text 		=> "text",
+            Self::Bool 		=> "bool",
+            Self::Timestamp => "timestamp",
+            Self::Link 		=> "link",
+        }
+    }
+}
+
+impl From<&str> for DataType {
+    fn from(s: &str) -> Self {
+        match s {
+            "int"       => Self::Int,
+            "float"     => Self::Float,
+            "text"      => Self::Text,
+            "bool"      => Self::Bool,
+            "timestamp" => Self::Timestamp,
+            "link"      => Self::Link,
+            _           => Self::Text,
+        }
+	}
+}
+
+impl fmt::Display for DataType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
 pub struct Field {
     name: String,
     label: String,
@@ -29,11 +74,25 @@ pub struct Field {
     is_virtual: bool, // vim at least thinks 'virtual' is reserved
 }
 
+impl fmt::Display for Field {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Field: name={} datatype={} virtual={} label={}",
+            self.name, self.datatype, self.is_virtual, self.label)
+    }
+}
+
+pub enum RelType {
+    HasA,
+    HasMany,
+    MightHave,
+    Unset,
+}
+
 pub struct Link {
     field: String,
-    reltype: String,
+    reltype: RelType,
     key: String,
-    map: String,
+    map: Option<String>,
     class: String,
 }
 
@@ -42,6 +101,13 @@ pub struct Class {
     label: String,
     fields: HashMap<String, Field>,
     links: HashMap<String, Link>,
+}
+
+impl fmt::Display for Class {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Class: class={} fields={} links={} label={} ",
+            self.class, self.fields.len(), self.links.len(), self.label)
+    }
 }
 
 pub struct Parser {
@@ -82,7 +148,115 @@ impl Parser {
     }
 
     fn add_class(&mut self, node: &roxmltree::Node) {
-        println!("Adding class {}", node.attribute("id").unwrap());
+
+        let name = node.attribute("id").unwrap(); // required
+
+        let label = match node.attribute((OILS_NS_REPORTER, "label")) {
+            Some(l) => l.to_string(),
+            None => name.to_string(),
+        };
+
+        let mut class = Class {
+            class: name.to_string(),
+            label: label,
+            fields: HashMap::new(),
+            links: HashMap::new(),
+        };
+
+        let mut field_array_pos = 0;
+
+        for child in node.children()
+            .filter(|n| n.node_type() == roxmltree::NodeType::Element) {
+
+            if child.tag_name().name() == "fields" {
+                for field_node in child.children()
+                    .filter(|n| n.node_type() == roxmltree::NodeType::Element)
+                    .filter(|n| n.tag_name().name() == "field") {
+
+                    self.add_field(&mut class, field_array_pos, &field_node);
+                    field_array_pos += 1;
+                }
+
+            } else if child.tag_name().name() == "links" {
+                for link_node in child.children()
+                    .filter(|n| n.node_type() == roxmltree::NodeType::Element)
+                    .filter(|n| n.tag_name().name() == "link") {
+
+                    self.add_link(&mut class, &link_node);
+                }
+            }
+        }
+
+        self.add_auto_fields(&mut class, field_array_pos);
+
+        println!("IDL Adding: {}", class);
+
+        self.classes.insert(class.class.to_string(), class);
+    }
+
+    fn add_auto_fields(&self, class: &mut Class, mut pos: usize) {
+
+        for field in AUTO_FIELDS {
+
+            class.fields.insert(field.to_string(), Field {
+                name: field.to_string(),
+                label: field.to_string(),
+                datatype: DataType::Bool,
+                i18n: false,
+                array_pos: pos,
+                is_virtual: true,
+            });
+
+            pos += 1;
+        }
+    }
+
+    fn add_field(&self, class: &mut Class, pos: usize, node: &roxmltree::Node) {
+
+        let label = match node.attribute((OILS_NS_REPORTER, "label")) {
+            Some(l) => l.to_string(),
+            None => "".to_string(),
+        };
+
+        let datatype: DataType =
+            match node.attribute((OILS_NS_REPORTER, "datatype")) {
+            Some(dt) => dt.into(),
+            None => DataType::Text,
+        };
+
+        let i18n: bool = match node.attribute((OILS_NS_PERSIST, "i18n")) {
+            Some(i) => i == "true",
+            None => false,
+        };
+
+        let is_virtual: bool = match node.attribute((OILS_NS_PERSIST, "virtual")) {
+            Some(i) => i == "true",
+            None => false,
+        };
+
+        let field = Field {
+            name: node.attribute("name").unwrap().to_string(),
+            label: label,
+            datatype: datatype,
+            i18n: i18n,
+            array_pos: pos,
+            is_virtual: is_virtual,
+        };
+
+        class.fields.insert(field.name.to_string(), field);
+    }
+
+    fn add_link(&self, class: &mut Class, node: &roxmltree::Node) {
+
+        let link = Link {
+            field: node.attribute("field").unwrap().to_string(),
+            reltype: RelType::HasA, // TODO
+            key: node.attribute("key").unwrap().to_string(),
+            map: None, // TODO
+            class: node.attribute("class").unwrap().to_string(),
+        };
+
+        class.links.insert(link.field.to_string(), link);
     }
 }
 
