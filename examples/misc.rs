d9280e14a19fe7a289c205759640d627e48e1403
Bill Erickson <berickxx@gmail.com>
2022-09-30 10:31:37 -0400

    DB handle, getops, idl fun
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- examples/misc.rs
+++ examples/misc.rs
@@ -6,6 +6,7 @@ use std::env;
 use json;
 
 fn main() -> Result<(), String> {
+    env_logger::init();
 
     let args: Vec<String> = env::args().collect();
     let mut opts = getopts::Options::new();
@@ -14,13 +15,19 @@ fn main() -> Result<(), String> {
 
     let params = opts.parse(&args[1..]).unwrap();
 
-    let db = DatabaseConnection::new_from_options(&params).to_shared();
+    let mut db = DatabaseConnection::new_from_options(&params);
+    db.connect()?;
+    let db = db.to_shared();
 
     let idl = eg::idl::Parser::parse_file("/openils/conf/fm_IDL.xml").to_shared();
 
     let translator = Translator::new(idl, db);
 
-    translator.search("aou", &json::object!{id: 1})?;
+    let results = translator.search("aou", &json::object!{id: 1})?;
+
+    for org in results {
+        println!("org = {org:?}");
+    }
 
     Ok(())
 }
--- src/idldb.rs
+++ src/idldb.rs
@@ -1,5 +1,5 @@
 ///! Tools for translating between IDL objects and Database rows.
-use log::error;
+use log::{trace, error};
 use json::JsonValue;
 use super::db;
 use super::idl;
@@ -23,7 +23,7 @@ impl Translator {
 
     pub fn search(&self, idlclass: &str, filter: &JsonValue) -> Result<Vec<JsonValue>, String> {
 
-        let results: Vec<JsonValue> = Vec::new();
+        let mut results: Vec<JsonValue> = Vec::new();
         let idl_parser = self.idl.borrow();
 
         let class = match idl_parser.classes().get(idlclass) {
@@ -33,33 +33,36 @@ impl Translator {
             }
         };
 
+        let tablename = match class.tablename() {
+            Some(t) => t,
+            None => {
+                return Err(format!(
+                    "Cannot query an IDL class that has no tablename: {idlclass}"));
+            }
+        };
+
         let select = self.compile_class_select(&class);
+        let filter = self.compile_class_filter(&class, filter)?;
 
         println!("compiled select: {select}");
+        println!("compiled filter: {filter}");
 
-        /*
-        let query_res = self.db.borrow().client().query(&query[..], &[]);
+        let query = format!("{select} FROM {tablename} {filter}");
+
+        let query_res = self.db.borrow_mut().client().query(&query[..], &[]);
 
         if let Err(e) = query_res {
             return Err(format!("DB query failed: {e}"));
         }
 
         for row in query_res.unwrap() {
-            results.push(self.row_to_idl(idlclass, &row)?);
+            results.push(self.row_to_idl(&class, &row)?);
         }
-        */
 
         Ok(results)
     }
 
     pub fn compile_class_select(&self, class: &idl::Class) -> String {
-
-        /*
-        if !query.is_object() {
-            return Err(format!("Translator query must be an object: {query:?}"));
-        }
-        */
-
         let mut sql = String::from("SELECT");
 
         for (name, field) in class.fields() {
@@ -71,7 +74,58 @@ impl Translator {
         String::from(&sql[0..sql.len() - 1]) // Trim final ","
     }
 
-    pub fn row_to_idl(&self, idlclass: &str, row: &pg::Row) -> Result<JsonValue, String> {
+    pub fn compile_class_filter(&self,
+        class: &idl::Class, filter: &JsonValue) -> Result<String, String> {
+
+        if !filter.is_object() {
+            return Err(format!(
+                "Translator class filter must be an object: {}", filter.dump()));
+        }
+
+        let mut sql = String::from("WHERE");
+
+        for (field, subq) in filter.entries() {
+            trace!("compile_class_filter adding filter on field: {field}");
+
+            sql += &format!(" {field}");
+
+            match subq {
+                JsonValue::Object(o) => {
+                    // TODO
+                },
+                JsonValue::Array(a) => {
+                    // TODO
+                },
+                JsonValue::Number(n) => {
+                    sql += &format!(" = {n}");
+                },
+                JsonValue::String(s) => {
+                    sql += &format!(" = QUOTE_LITERAL({})", s);
+                },
+                JsonValue::Short(s) => {
+                    sql += &format!(" = QUOTE_LITERAL({})", s);
+                }
+                JsonValue::Boolean(b) => {
+                    sql += &format!(" IS {b}");
+                },
+                JsonValue::Null => {
+                    sql += " IS NULL";
+                },
+            }
+        }
+
+        Ok(sql)
+    }
+
+    pub fn row_to_idl(&self, class: &idl::Class, row: &pg::Row) -> Result<JsonValue, String> {
+
+        let mut idx = 0;
+        for (name, field) in class.fields() {
+            let value: &str = row.get(idx);
+            trace!("Row value {field}={value}");
+            idx += 1;
+        }
+
         Ok(JsonValue::Null)
     }
 }
