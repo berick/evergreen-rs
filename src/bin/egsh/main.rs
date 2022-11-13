6cd2577aef60fdc3e1144dab8301a1ea58166d6e
Bill Erickson <berickxx@gmail.com>
2022-11-13 09:19:18 -0500

    egsh
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- src/bin/egsh/main.rs
+++ src/bin/egsh/main.rs
@@ -1,20 +1,25 @@
 use std::env;
+use std::io;
+use std::io::Write;
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use std::time::Duration;
+use json::JsonValue;
 use getopts;
-use indicatif;
+use indicatif::ProgressBar;
 use opensrf as osrf;
 use evergreen as eg;
 use eg::idl;
 use eg::idldb;
+use eg::idldb::IdlClassSearch;
 use eg::db::DatabaseConnection;
 use osrf::conf;
 use osrf::client::Client;
 
-const PROMPT: &str = "egsh# ";
+const PROMPT: &str = "egsh#";
 const DEFAULT_IDL_PATH: &str = "/openils/conf/fm_IDL.xml";
 
 fn main() -> Result<(), String> {
@@ -22,11 +27,34 @@ fn main() -> Result<(), String> {
     let (conf, _) = osrf::init_with_options("service", &mut opts)?;
 
     let mut shell = Shell::setup(conf.into_shared(), &mut opts);
+    shell.main_loop();
 
     Ok(())
 }
 
 struct SpinnerThread {
+    stop: Arc<AtomicBool>,
+}
+
+impl SpinnerThread {
+
+    fn start(&mut self) {
+        let mut spinner = ProgressBar::new_spinner();
+
+        loop {
+            // Start with the sleep so the spinner only appears if the
+            // request is actually taking enough time for a human to notice.
+            thread::sleep(Duration::from_millis(50));
+
+            if self.stop.load(Ordering::SeqCst) {
+                // Main thread said to cut it out.
+                spinner.finish();
+                break;
+            }
+
+            spinner.tick();
+        }
+    }
 }
 
 struct Shell {
@@ -34,9 +62,11 @@ struct Shell {
     db: Option<Rc<RefCell<DatabaseConnection>>>,
     db_translator: Option<idldb::Translator>,
     config: Arc<conf::Config>,
+    progress_flag: Arc<AtomicBool>,
 }
 
 impl Shell {
+
     fn setup(config: Arc<conf::Config>, opts: &mut getopts::Options) -> Shell {
         opts.optflag("", "with-database", "Open Direct Database Connection");
         opts.optopt("", "idl-file", "Path to IDL file", "IDL_PATH");
@@ -58,6 +88,7 @@ impl Shell {
             idl,
             db: None,
             db_translator: None,
+            progress_flag: Arc::new(AtomicBool::new(false)),
         };
 
         if params.opt_present("with-database") {
@@ -79,6 +110,114 @@ impl Shell {
 
         self.db = Some(db);
         self.db_translator = Some(translator);
+    }
+
+    fn start_progress(&mut self) {
+        let flag = self.progress_flag.clone();
+        thread::spawn(|| SpinnerThread { stop: flag }.start());
+    }
+
+    fn stop_progress(&mut self) {
+        self.progress_flag.store(true, Ordering::SeqCst);
+    }
+
+    fn main_loop(&mut self) {
+        loop {
+            print!("{PROMPT} ");
+            io::stdout().flush().unwrap();
+            if !self.read_one_command() {
+                break;
+            }
+        }
+    }
+
+    /// Returns true if we should keep going.
+    fn read_one_command(&mut self) -> bool {
+
+        let mut user_input = String::new();
+
+        io::stdin().read_line(&mut user_input);
+
+        if user_input.len() == 0 {
+            return true;
+        }
+
+        let user_input = user_input.trim();
+
+        let parts: Vec<&str> = user_input.split(" ").collect();
+
+        let command = parts[0].to_lowercase();
+
+        match command.as_str() {
+            "stop" | "quit" | "exit" => {
+                return false;
+            }
+            "idl" => self.idl_query(&parts[1..]),
+            _ => eprintln!("Unknown command: {command}"),
+        }
+
+        return true;
+    }
+
+    fn idl_query(&mut self, parts: &[&str]) {
+
+        if parts.len() < 3 {
+            eprintln!("'idl' command requires additional parameters: {parts:?}");
+            return;
+        }
+
+        match parts[0] {
+            "get" => self.idl_get(&parts[1..]),
+            _ => {
+                eprintln!("Could not parse idl query command: {parts:?}");
+                return;
+            }
+        }
+    }
+
+    fn idl_get(&mut self, parts: &[&str]) {
+
+        let mut translator = match self.db_translator.as_mut() {
+            Some(t) => t,
+            None => {
+                eprintln!("no translator");
+                return;
+            }
+        };
+
+        if parts.len() < 2 {
+            eprintln!("'idl get' command requires additional parameters: {parts:?}");
+            return;
+        }
+
+        let idl_class = match self.idl.classes().get(parts[0]) {
+            Some(c) => c,
+            None => {
+                eprintln!("No such IDL class: {}", parts[0]);
+                return;
+            }
+        };
+
+        let pkey_field = match idl_class.pkey() {
+            Some(f) => f,
+            None => {
+                eprintln!(
+                    "IDL class {} has no pkey value and cannot be queried",
+                    idl_class.classname()
+                );
+                return;
+            }
+        };
+
+        let mut filter = JsonValue::new_object();
+        filter.insert(&pkey_field, parts[1]);
+
+        let mut search = IdlClassSearch::new(parts[0]);
+        search.set_filter(filter);
+
+        for org in translator.idl_class_search(&search) { // ? todo
+            println!("{org:?}");
+        }
     }
 }
 
